b50f0cfb31fa8aa8746b72c6a34b8d27c880b3c5
9639_ginf (Student) <s_9639_ginf@PEDAGOGIQUE.LAN>
2020-01-13 14:25:53 +0100

    InitializeGit

---
+++ Assets/Code/Scripts/LayoutScript.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChangeNameClass : MonoBehaviour
+{
+	#region Declarations public
+
+	#endregion
+
+	#region Declarations private
+
+	#endregion
+
+	#region Declarations Event Args
+
+	#endregion
+
+	#region Declarations Event Handler
+
+	#endregion
+
+	#region Declarations Event Call
+
+	#endregion
+
+	#region Functions Unity
+	private void Awake()
+	{
+		#region Initialize
+
+		#endregion
+	}
+
+	private void Start()
+	{
+		#region Initialize
+
+		#endregion
+	}
+
+	private void Update()
+	{
+		#region Movement
+
+		#endregion
+
+		#region Actions
+
+		#endregion
+
+		#region Timer
+
+		#endregion
+	}
+
+	private void FixedUpdate()
+	{
+		#region Movement
+
+		#endregion
+
+		#region Actions
+
+		#endregion
+
+		#region Timer
+
+		#endregion
+	}
+	#endregion
+
+	#region Helper
+
+	#endregion
+}
