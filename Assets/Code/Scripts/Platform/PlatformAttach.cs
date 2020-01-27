b94d1423fe3dd88c884635d332f7d7b812b003d2
9639_ginf (Student) <s_9639_ginf@PEDAGOGIQUE.LAN>
2020-01-27 08:21:44 +0100

    correction platform + debug

---
+++ Assets/Code/Scripts/Platform/PlatformAttach.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformAttach : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.tag == "Player")
+		{
+			collision.gameObject.transform.parent = transform;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+			collision.gameObject.transform.parent = null;
+		}
+	}
+}
